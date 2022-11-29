0014cb867b5e21c40fc8c28973d6a5fff364aa93
CHEVRIER Titouan 42 <tchevrie@student.42.fr>
2022-11-29 21:14:19 +0100

    FdF

--- fdf.c
+++ fdf.c
@@ -6,33 +6,45 @@
 /*   By: tchevrie <tchevrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:20:35 by tchevrie          #+#    #+#             */
-/*   Updated: 2022/11/29 19:31:59 by tchevrie         ###   ########.fr       */
+/*   Updated: 2022/11/29 21:09:11 by tchevrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	main(void)
+void	ft_colored_window(int w, int h)
 {
-	int		color;
-	int		width;
-	int		height;
 	void	*mlx_ptr;
 	void	*win_ptr;
+	int		color;
+	size_t	x;
+	size_t	y;
 
-	color = rgb(17, 127, 122);
-	ft_printf("La couleur vaut %x\n", color);
-	// width = 750;
-	// height = 750;
-	// mlx_ptr = mlx_init();
-	// win_ptr = mlx_new_window(mlx_ptr, width, height, "42");
-	// while (width > 0 && height > 0)
-	// {
-	// 	mlx_pixel_put(mlx_ptr, win_ptr, width, height, color);
-	// 	color++;
-	// 	width--;
-	// 	height--;
-	// }
-	rrgb(color);
-	// mlx_loop(mlx_ptr);
+	w = 255 * (w / 255);
+	h = 255 * (h / 255);
+	if (w < 255)
+		w = 255;
+	if (h < 255)
+		h = 255;
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, w, h, "42");
+	y = -1;
+	while (++y < h)
+	{
+		x = -1;
+		while (++x < w)
+		{
+			color = rgb(x / (w / 255), y / (h / 255), 255 - x / (w / 255));
+			mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
+		}
+	}
+	mlx_loop(mlx_ptr);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+		return (ft_printf("Imbecile", 0));
+	ft_colored_window(ft_atoi(argv[1]), ft_atoi(argv[2]));
+	return (0);
 }
